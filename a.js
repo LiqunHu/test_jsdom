7e8f0793bd85e281f739b0d8ef45331fb4b45aed
LiqunHu <huliquns@126.com>
2024-05-22 13:13:29 +0800

    update

--- a.js
+++ a.js
@@ -1,26 +1,29 @@
+const fs = require('fs')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
-let text = `<!DOCTYPE html><p>Hello world</p>`
+let web = fs.readFileSync('./test.html', 'utf-8')
+// let web = `<!DOCTYPE html><p>Hello world</p>`
+// console.log(web)
 
-const dom = new JSDOM(text, { includeNodeLocations: true })
-let ele = dom.window.document.querySelector('p')
-console.log(ele)
-let offset = dom.nodeLocation(ele)
-console.log(ele.outerHTML)
-console.log(text.slice(offset.startTag.startOffset, offset.startTag.endOffset))
-console.log(text.slice(offset.endTag.startOffset, offset.endTag.endOffset))
-console.log(text.slice(offset.startOffset, offset.endOffset))
-console.log(ele.innerHTML)
-console.log(
-  text.slice(
-    offset.startOffset +
-      (offset.startTag.endOffset - offset.startTag.startOffset),
-    offset.endOffset - (offset.endTag.endOffset - offset.endTag.startOffset),
-  ),
-)
+const dom = new JSDOM(web, { includeNodeLocations: true })
+// let ele = dom.window.document.querySelector('p')
+// console.log(ele)
+// let offset = dom.nodeLocation(ele)
+// console.log(ele.outerHTML)
+// console.log(web.slice(offset.startTag.startOffset, offset.startTag.endOffset))
+// console.log(web.slice(offset.endTag.startOffset, offset.endTag.endOffset))
+// console.log(web.slice(offset.startOffset, offset.endOffset))
+// console.log(ele.innerHTML)
+// console.log(
+//   web.slice(
+//     offset.startOffset +
+//       (offset.startTag.endOffset - offset.startTag.startOffset),
+//     offset.endOffset - (offset.endTag.endOffset - offset.endTag.startOffset),
+//   ),
+// )
 const document = dom.window.document
 var result = document.evaluate(
-  '//p',
+  '//div[@class="js-yearly-contributions"]/div[@class="position-relative"]/h2',
   document,
   null,
   dom.window.XPathResult.ANY_TYPE,
